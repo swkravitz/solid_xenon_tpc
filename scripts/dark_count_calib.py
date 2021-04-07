b1edd475e5b1ff355649aa9164cdc8fce721970d
rhingorani10 <rhingorani@lbl.gov>
2021-04-07 16:20:51 -0700

    find the max of a trace and integrate around it within a fixed window.

--- scripts/dark_count_calib.py
+++ scripts/dark_count_calib.py
@@ -39,7 +39,9 @@ mpl.rcParams['legend.fontsize']='small'
 mpl.rcParams['figure.autolayout']=True
 mpl.rcParams['figure.figsize']=[16.0,12.0]
 
-channel_0=np.fromfile("../../101320/chanH_50V_16mV/wave0.dat", dtype="int16")
+data_dir = "/home/xaber/caen/wavedump-3.8.2/data/040721/dark_count_all_channel_Cu_rod_110.0/"
+channel_name = "0"
+channel_0=np.fromfile(data_dir+"wave"+channel_name+".dat", dtype="int16")
 
 #channel_0=np.fromfile("../../072320/chB_darkcount_calib_50V_-6.1mV/wave0.dat", dtype="int16")
 
@@ -48,9 +50,10 @@ channel_0=np.fromfile("../../101320/chanH_50V_16mV/wave0.dat", dtype="int16")
 
 
 
-vscale=(2000.0/16384.0)
-wsize=2048 # Number of samples per waveform
+vscale=(2000.0/16384.0)/4. # smaller digitizer range
+wsize=1030 # Number of samples per waveform
 V=vscale*channel_0
+print("record length: ", len(V))
 # Ensure we have an integer number of events
 V=V[:int(len(V)/wsize)*wsize]
 n_channels=2 # including sum
@@ -111,7 +114,7 @@ for i in range(0, int(v_matrix.shape[0])):
         
 	# Look for events with S1 and S2 from summed channel
     s1_window = int(0.2/tscale)
-    s2_window = int(0.25/tscale)
+    s2_window = int(0.15/tscale)
     s1_thresh = 400
     s1_range_thresh = 10
     s2_thresh = 0.
@@ -132,21 +135,39 @@ for i in range(0, int(v_matrix.shape[0])):
     s1_found=False
     s2_found=False
 
-    sum_baseline=np.mean(v4_matrix[i,int(1.4/tscale):int(1.7/tscale)]) #avg 0.3 us, later in acquisition since there may be odd noise earlier?
+    sum_baseline=np.mean(v4_matrix[i,int(0.2/tscale):int(0.5/tscale)]) #avg 0.3 us, later in acquisition since there may be odd noise earlier?
     sum_data=v4_matrix[i,:]-sum_baseline
     
     # Do a moving average (sum) of the waveform with different time windows for s1, s2
 	# Look for where this value is maximized
 	# Look for the s2 using a moving average (sum) of the waveform over a wide window
-    t_min_search=int(1.7/tscale)
-    t_max_search=int(3.5/tscale)
+    t_min_search=int(0.5/tscale)
+    t_max_search=int(1.5/tscale)
     t_offset=int(0.00/tscale)
-    s2_max_ind, s2_max=pulse_finder_area(sum_data,t_min_search,t_max_search,s2_window)
+    #s2_max_ind, s2_max=pulse_finder_area(sum_data,t_min_search,t_max_search,s2_window)
+    
+    # Find max of waveform, integrate window w/in range nearby
+    t_before = int(0.05/tscale)
+    t_after = int(0.15/tscale)
+    t_baseline = int(0.15/tscale)
+    s2_window = t_before+t_after
+    s2_max_ind = np.argmax(sum_data)
+    s2_max = sum_data[s2_max_ind]
+    #s2_start = s2_max_ind-t_before
+    #s2_end = s2_max_ind+t_after
+    #if s2_start<0 or s2_end>wsize: continue
+    s2_start = np.maximum(0, s2_max_ind-t_before)
+    s2_end = np.minimum(wsize, s2_max_ind+t_after)
+    baseline_start = np.maximum(0, s2_start-t_baseline)
+    baseline_correct = np.mean(sum_data[baseline_start:s2_start])
+    s2_area = np.sum(sum_data[s2_start:s2_end]-baseline_correct)
+    s2_max_ind = s2_start
+    
     #print(s2_max)
   
 	
     s2_found=True#s2_max>s2_thresh
-    s2_area=s2_max
+    #s2_area=s2_max
 	
     ratio=-1
 
@@ -171,7 +192,7 @@ for i in range(0, int(v_matrix.shape[0])):
         pl.rc('ytick', labelsize=25)
         
         ax=pl.subplot2grid((1,1),(0,0),colspan=2)
-        pl.plot(t_matrix[i,:],sum_data,'blue')
+        pl.plot(t_matrix[i,:],sum_data-baseline_correct,'blue')
         pl.grid(b=True,which='major',color='lightgray',linestyle='--',lw=1)
         #pl.xlim([1.5, 2.5])
         pl.xlim([0, 4])
@@ -186,6 +207,8 @@ for i in range(0, int(v_matrix.shape[0])):
         if s2_found:
             #ax.axvspan(s2_start_pos*tscale, s2_end_pos*tscale, alpha=0.5, color='blue')
             ax.axvspan(s2_max_ind*tscale, (s2_max_ind+s2_window)*tscale, alpha=0.3, color='green')
+            ax.text((s2_max_ind+s2_window)*tscale, 0.9 * ax.get_ylim()[1], '{:.1f} mv*samples'.format(s2_area),
+                    fontsize=24)
         if s1_found:
             ax.axvspan(s1_start_pos*tscale, s1_end_pos*tscale, alpha=0.5, color='green')
             
@@ -211,19 +234,24 @@ print("S2 height mean: ", np.mean(s2_height_array[s2_found_array]))
 #pl.show()
 def gaussian(x, mean, std,a):
     return a*np.exp(-((x-mean)/std)**2)
+bins = np.linspace(-20, 100, 150)
+s2_start = 8
+s2_end = 18
+s2_start_bin = np.digitize(s2_start, bins)
+s2_end_bin = np.digitize(s2_end, bins)
 pl.figure()
-h,b,_=pl.hist(s2_area_array[s2_found_array],bins=150,range=(0,800))
+h,b,_=pl.hist(s2_area_array[s2_found_array],bins=bins)
 bin_centers = b[:-1] + np.diff(b)/2
-#popt, _ = curve_fit(gaussian,bin_centers[18:55],h[18:55], p0=[270,80,450])
+popt, _ = curve_fit(gaussian,bin_centers[s2_start_bin:s2_end_bin],h[s2_start_bin:s2_end_bin], p0=[270,80,450])
 #print("bin_centers:", bin_centers)
 #print("h:", h)
-#print("popt:",popt)
-#pl.plot(bin_centers, gaussian(bin_centers, *popt), label ='fit')
+print("popt:",popt)
+pl.plot(bin_centers, gaussian(bin_centers, *popt), label ='fit')
 
 pl.grid(b=True,which='major',color='lightgray',linestyle='--')
 #pl.yscale("log")
 #pl.axvline(x=popt[0],ls='--',color='r')
-#pl.text(0.3,0.8,'Gaussian Mean: {0:g}'.format(popt[0]),transform=pl.gca().transAxes)
+pl.text(0.3,0.8,'Gaussian Mean: {0:g}'.format(popt[0]),transform=pl.gca().transAxes)
 #pl.text(0.3,0.7,'std: {0:g}'.format(popt[1]),transform=pl.gca().transAxes)
 #pl.text(0.3,0.6,'Amplitude: {0:g}'.format(popt[2]),transform=pl.gca().transAxes)
 pl.xlabel("Pulse Area")
@@ -243,5 +271,5 @@ pl.xlabel("Pulse Area")
 #pl.scatter(s2_area_array[s2_found_array],s2_width_array[s2_found_array])
 #pl.xlabel("S2 area")
 #pl.ylabel("S2 width (us)")
-
+pl.savefig(data_dir+"dark_counts_pulse_area_hist_channel_"+channel_name+".png")
 pl.show()
