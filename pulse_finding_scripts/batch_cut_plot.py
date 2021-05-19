640e2b3a1e8087fdf2d737854728b51504bf0ed1
maquechen <maquechen@gmail.com>
2021-05-19 11:19:09 -0700

    Simplify batch script, use rq_generate and cut_plot as modules

--- pulse_finding_scripts/batch_cut_plot.py
+++ pulse_finding_scripts/batch_cut_plot.py
@@ -6,10 +6,19 @@ from glob import glob
 
 with open("batch_path.txt", 'r') as path:
     path_text = path.read()
-list_dir = glob(path_text)
-for dir in list_dir:
-    if ("spe" in dir) or ("dark" in dir): list_dir.remove(dir)
-print('Data to process:', '\n'.join(list_dir))
+
+list_dir = glob(path_text+"*/")
+
+
+for i in reversed(range(len(list_dir))):
+    if ("spe" in list_dir[i]) or ("dark" in list_dir[i]): list_dir.pop(i)
+
+print("\n Data to process:\n")
+print('\n'.join(list_dir))
+print("\n Check the data list above, if not correct, press q then Enter. Otherwise, press any other key to start\n")
+
+flag = input()
+if flag == "q": exit()
 
 for data_dir in list_dir:
     print("Now start to process:"+data_dir)
